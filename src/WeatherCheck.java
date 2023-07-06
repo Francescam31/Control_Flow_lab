f5ab7ce55be26dfb7df92a10b46fc361443a54cb
Francesca Marshall <francescam3105@gmail.com>
2023-07-06 17:17:57 +0100

    finished the weather check task

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,5 +1,16 @@
 public class WeatherCheck {
     public static void main(String[] args) {
-        
+        int temperature = 1;
+        boolean currentlyRaining = false;
+
+        if (temperature > 20 && !currentlyRaining) {
+            System.out.println("sunscreen and shorts");
+        } else if (temperature >= 10 && currentlyRaining) {
+            System.out.println("take an umbrella");
+        } else if (temperature >= 10 && !currentlyRaining) {
+            System.out.println("wear a light jacket");
+        } else if (temperature < 10 || currentlyRaining) {
+            System.out.println("Just stay inside");
+        }
     }
 }
