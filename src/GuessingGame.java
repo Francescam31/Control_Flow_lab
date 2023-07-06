dd01ebd01a386cb0a2ecb5210a1f0a9f831e0448
Francesca Marshall <francescam3105@gmail.com>
2023-07-06 16:39:24 +0100

    Completed the guessing game

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,6 +1,14 @@
 public class GuessingGame {
     public static void main(String[] args) {
         int secretNumber = 3;
-        int guess = 4;
+        int guess = 3;
+
+        if (secretNumber == guess){
+            System.out.println("Correct");
+        } else if (secretNumber < guess){
+            System.out.println("Too high");
+        } else {
+            System.out.println("Too low");
+        }
     }
 }
